77a2fcf7038850305065d5d5446e3ad377a84285
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-01-25 17:19:04 +0530

    Added doctors section

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row ">
           {/* about img */}
           <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
-            <img src={aboutImg} alt="" />
+            <img src={aboutImg} className="rounded-[30px]" alt="" />
             <div className="absolute z-20 bottom-4 w-[250px] md:w-[-300px] right-[-30%] md:right-[-7%] lg:right-[22%]">
               <img src={aboutCardImg} alt="" />
             </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,9 +6,15 @@ import heroImg03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
+import featureImg from "../assets/images/feature-img.png";
+import videoIcon from "../assets/images/video-icon.png";
+import avatarIcon from "../assets/images/avatar-icon.png";
+
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
 import About from "../components/About/About";
+import ServicesList from "../components/Services/ServicesList";
+import DoctorList from "../components/Doctors/DoctorList";
 
 const Home = () => {
   return (
@@ -92,7 +98,6 @@ const Home = () => {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
-
             <div className="py-[30px] px-5">
               <div className="flex items-center justify-center">
                 <img src={icon01} alt="" />
@@ -161,7 +166,6 @@ const Home = () => {
                 </Link>
               </div>
             </div>
-
           </div>
         </div>
       </section>
@@ -172,6 +176,93 @@ const Home = () => {
 
       {/* service section */}
 
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our medical services</h2>
+            <p className="text_para text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
+              adipisicing elitnobis.
+            </p>
+          </div>
+
+          <ServicesList />
+        </div>
+      </section>
+
+      {/* feature section */}
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/* feature content */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get virtual treatment <br /> anytime
+              </h2>
+              <ul className="pl-4">
+                <li className="text_para">1. Schedule the appointment</li>
+                <li className="text_para">
+                  2. Search for your physician here, and contact their office
+                </li>
+                <li className="text_para">
+                  3. View our physicians who are accepting new patients, use the
+                  online scheduling tool to select an appointment time
+                </li>
+              </ul>
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
+
+            {/* feature img */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} className="w-3/4 rounded-[30px]" alt="" />
+
+              <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tue, 24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
+                      10:00 AM
+                    </p>
+                  </div>
+                  <span className="w-5 h-5 lg:w-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:px-[9px]">
+                    <img src={videoIcon} alt="" />
+                  </span>
+                </div>
+
+                <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-[8px]">
+                  Consultation
+                </div>
+
+                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="" />
+                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
+                    Kamal Perera
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Doctors section */}
+      <section>
+        <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our Doctors</h2>
+            <p className="text_para text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
+              adipisicing elitnobis.
+            </p>
+          </div>
+
+          <DoctorList />
+        </div>
+      </section>
     </>
   );
 };
