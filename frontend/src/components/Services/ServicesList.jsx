38cd3deb51b497fbe41537843897d62e2109736f
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-01-26 09:29:38 +0530

    Added services page

--- frontend/src/components/Services/ServicesList.jsx
+++ frontend/src/components/Services/ServicesList.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-import {services} from "./../../assets/data/services";
+import { services } from "./../../assets/data/services";
 import ServicesCard from "./ServicesCard";
 
 const ServicesList = () => {
-  return <div className="grid gris-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
-    {services.map((item, index) => (
+  return (
+    <div className="grid gris-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+      {services.map((item, index) => (
         <ServicesCard item={item} index={index} key={index} />
-    ))}
-  </div>;
+      ))}
+    </div>
+  );
 };
 
 export default ServicesList;
