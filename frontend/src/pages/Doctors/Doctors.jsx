8f09621fafaa95de0124efc4006f35e2239a0374
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-01-26 12:02:45 +0530

    Update Doctors.jsx

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import DoctorCard from "./../../components/Doctors/DoctorCard";
+import { doctors } from "./../../assets/data//doctors";
+import Testimonial from "../../components/Testimonial/Testimonial";
 
 const Doctors = () => {
   return (
-    <div>Doctors</div>
-  )
-}
+    <>
+      <section className="bg-[#fff9EA]">
+        <div className="container text-center">
+          <h2 className="heading">Find a Doctor</h2>
+          <div className="max-w-[570px] mt-[30px] mx-auto bg-[#446ca92c] rounded-md flex items-center justify-between">
+            <input
+              type="Search"
+              className="py-4 pl-4 pr-2 bg:transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor"
+              placeholder="Search Doctor"
+            />
+            <button className="btn mt-0 rounded-[0px] rounded-r-md">
+              Search
+            </button>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {doctors.map((doctor) => (
+              <DoctorCard key={doctor.id} doctor={doctor} />
+            ))}
+          </div>
+        </div>
+      </section>
 
-export default Doctors
+      <section>
+        <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">What our patients say</h2>
+            <p className="text_para text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
+              adipisicing elitnobis.
+            </p>
+          </div>
+          <Testimonial />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Doctors;
