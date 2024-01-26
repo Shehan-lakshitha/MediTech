5221c3385b8595b54501fa791e3c15eeae006531
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-01-26 17:08:27 +0530

    Done about section

--- frontend/src/pages/Doctors/DoctorsAbout.jsx
+++ frontend/src/pages/Doctors/DoctorsAbout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { formateDate } from "../../utils/formateDate";
 
 const DoctorsAbout = () => {
   return (
@@ -30,28 +31,61 @@ const DoctorsAbout = () => {
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                23 June, 2005
+                {formateDate("12-04-2005")} - {formateDate("12-04-2010")}
               </span>
-              <p className="text-[16px] leading-6 font-medium text-textColor">
+              <p className="text-[15px] leading-6 font-medium text-textColor">
                 PHD Surgeaon
               </p>
             </div>
             <p className="text-[14px] leading-5 font-medium text-textColor">
-                New Apollo Hospital, Colombo.
-              </p>
+              New Apollo Hospital, Colombo.
+            </p>
           </li>
           <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
             <div>
               <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
-                23 June, 2005
+                {formateDate("12-04-2013")} - {formateDate("12-04-2018")}
               </span>
-              <p className="text-[16px] leading-6 font-medium text-textColor">
+              <p className="text-[15px] leading-6 font-medium text-textColor">
                 PHD Surgeaon
               </p>
             </div>
             <p className="text-[14px] leading-5 font-medium text-textColor">
-                New Apollo Hospital, Colombo.
-              </p>
+              New Apollo Hospital, Colombo.
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold ">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded-[10px] bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formateDate("12-04-2005")} - {formateDate("12-04-2023")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeaon
+            </p>
+
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, Colombo.
+            </p>
+          </li>
+          <li className="p-4 rounded-[10px] bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formateDate("12-04-2005")} - {formateDate("12-04-2023")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeaon
+            </p>
+
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, Colombo.
+            </p>
           </li>
         </ul>
       </div>
