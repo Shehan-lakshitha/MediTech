a513425e57565dddce5cc4a21714777c7c501360
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-01-27 12:32:05 +0530

    Added sidepanel of doctor details

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -3,6 +3,7 @@ import doctorImg from "../../assets/images/doctor-img02.png";
 import starIcon from "../../assets/images/Star.png";
 import DoctorsAbout from "./DoctorsAbout";
 import Feedback from "./Feedback";
+import SidePanel from "./SidePanel";
 
 const DoctorDetails = () => {
   const [tab, setTab] = useState("about");
@@ -67,6 +68,10 @@ const DoctorDetails = () => {
                     tab == 'feedback' && <Feedback />
                   }
                 </div>
+          </div>
+
+          <div>
+            <SidePanel />
           </div>
         </div>
       </div>
