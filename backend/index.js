c7fe093795a9f8487178c93bb477e147a2be03f1
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-06-07 09:43:24 +0530

    Permissions

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,24 @@ app.use(cors(
     }
 ))
 
+module.exports = (req, res) => {
+    //set header first to allow request or origin domain (value can be different)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+ //Preflight CORS handler
+    if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: "OK"
+        }))
+    }
+
+}
+
 // const corsOptions = {
 //     origin:true,
 // }
@@ -50,15 +68,6 @@ const connectDB = async()=>{
 app.use(express.json());
 app.use(cookieParser());
 //app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://medi-tech-ochre.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/doctors', doctorRoute);
