0595258e3f89faac1b9cf9893b14255d06ba13b6
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-06-07 09:48:22 +0530

    bb

--- backend/index.js
+++ backend/index.js
@@ -16,32 +16,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors(
-    {
-        origin: 'https://medi-tech-ochre.vercel.app/',
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-        optionsSuccessStatus: 200
-    }
-))
-
-module.exports = (req, res) => {
-    //set header first to allow request or origin domain (value can be different)
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-
-//---- other code
-
- //Preflight CORS handler
-    if(req.method === 'OPTIONS') {
-        return res.status(200).json(({
-            body: "OK"
-        }))
-    }
-
-}
 
 // const corsOptions = {
 //     origin:true,
