31c062fa01737f2a49105621fcbfb9f15b60bc6e
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-06-07 12:13:08 +0530

    Added URL to deploy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 //export const BASE_URL = 'http://localhost:5000/api/v1';
-export const BASE_URL = 'http://meditech-app.azurewebsites.net/api/v1';
+export const BASE_URL = 'https://meditech.onrender.com/api/v1';
 export const token = localStorage.getItem('token');
